4c7406d5d64a09c40a2e793623181b6c41b48274
Rob <robertlopezca@gmail.com>
2021-01-17 20:47:08 -0800

    Added more CSS

--- src/View/ViewItem.jsx
+++ src/View/ViewItem.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ItemWrapper = styled.div`
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 55px;
+`;
 
 const Item = ({ item }) => {
   const stringArr = item.itemName.split(' ');
@@ -15,20 +22,23 @@ const Item = ({ item }) => {
     }
   }
 
+  let ratingCount = `(${item.ratingCount})`;
   return (
     <div>
-      <img src={item.photoURL} alt="" />
-      <div className="flex-item-content">
-        <div className="item-name">
-          <div className="item-brand">{item.brandName}</div>
-          {stringOne.join(' ')}
-          <br />
-          {stringTwo.join(' ')}
+      <ItemWrapper>
+        <img src={item.photoURL} alt="" />
+        <div className="flex-item-content">
+          <div className="item-name">
+            <div className="item-brand">{item.brandName}</div>
+            {stringOne.join(' ')}
+            <br />
+            {stringTwo.join(' ')}
+          </div>
+          <label className="item-rating">{item.itemRating}</label>
+          <label className="item-rating-count">{ratingCount}</label>
+          <div className="item-price">{item.itemPrice}</div>
         </div>
-        <div className="item-rating">{item.itemRating}</div>
-        <div className="item-rating-count">{item.ratingCount}</div>
-        <div className="item-price">{item.itemPrice}</div>
-      </div>
+      </ItemWrapper>
     </div>
   );
 };
