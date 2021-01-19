3a24dac9f89147f64d9416468c4836d94d846cdc
Rob <robertlopezca@gmail.com>
2021-01-19 14:15:16 -0800

    added button hover effect with styled components

--- src/components/ViewMain.jsx
+++ src/components/ViewMain.jsx
@@ -14,7 +14,17 @@ const ButtonStyleShow = styled.div`
   height: 40px;
   bottom: initial;
   background: #f9f8f6;
-  box-shadow: 0 0.4rem 0.4rem 0 rgba(12,11,8,.2);
+  box-shadow: 1 0.4rem 0.4rem 0 rgba(12,11,8,.2);
+  border: none;
+  outline: none;
+`;
+
+const ButtonStyleShowHover = styled.div`
+  border-radius: 50%;
+  height: 40px;
+  bottom: initial;
+  background: #f9f8f6;
+  box-shadow: 0 0.6rem 0.6rem 0 rgba(12,11,8,.08);
   border: none;
   outline: none;
 `;
@@ -29,22 +39,56 @@ const ButtonStyleHide = styled.div`
 const ViewMain = ({ data }) => {
   const [page, setPage] = useState(0);
   const [valid, setValid] = useState([1, 2, 3, 4, 5, 6]);
+  const [hover, setHover] = useState(false);
 
   useEffect(() => {
     if (page === 0) {
       setValid([1, 2, 3, 4, 5, 6]);
+      setHover(false);
     } else {
       setValid([6, 7, 8, 9, 10, 11]);
+      setHover(false);
     }
   }, [page]);
+
+  const hoverOn = () => {
+    setHover(true);
+  };
+
+  const hoverOff = () => {
+    setHover(false);
+  };
 
   let rightButton;
   let leftButton;
 
-  if (page === 0) {
+  if (page === 0 && hover) {
+    rightButton = (
+      <ButtonStyleShowHover>
+        <button
+          onMouseEnter={hoverOn}
+          onMouseLeave={hoverOff}
+          className="btn"
+          type="button"
+          onClick={() => {
+            setPage(1);
+          }}
+        >
+          <i className="fa fa-long-arrow-right" />
+        </button>
+      </ButtonStyleShowHover>
+    );
+    leftButton = (
+      <ButtonStyleHide>
+        <button type="button"> </button>
+      </ButtonStyleHide>
+    );
+  } else if (page === 0 && !hover) {
     rightButton = (
       <ButtonStyleShow>
         <button
+          onMouseEnter={hoverOn}
+          onMouseLeave={hoverOff}
           className="btn"
           type="button"
           onClick={() => {
@@ -60,10 +104,33 @@ const ViewMain = ({ data }) => {
         <button type="button"> </button>
       </ButtonStyleHide>
     );
+  } else if (page === 1 && hover) {
+    leftButton = (
+      <ButtonStyleShowHover>
+        <button
+          onMouseEnter={hoverOn}
+          onMouseLeave={hoverOff}
+          className="btn"
+          type="button"
+          onClick={() => {
+            setPage(0);
+          }}
+        >
+          <i className="fa fa-long-arrow-left" />
+        </button>
+      </ButtonStyleShowHover>
+    );
+    rightButton = (
+      <ButtonStyleHide>
+        <button type="button"> </button>
+      </ButtonStyleHide>
+    );
   } else {
     leftButton = (
       <ButtonStyleShow>
         <button
+          onMouseEnter={hoverOn}
+          onMouseLeave={hoverOff}
           className="btn"
           type="button"
           onClick={() => {
