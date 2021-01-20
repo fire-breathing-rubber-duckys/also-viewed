44043181d7a6b012b7bcfbc2100427734bde3f6c
Rob <robertlopezca@gmail.com>
2021-01-20 10:23:53 -0800

    added back API tests to check circleCI

--- test/api.test.js
+++ test/api.test.js
@@ -1,27 +1,27 @@
-// const request = require('supertest');
-// const mongoose = require('mongoose');
-// const app = require('../server/index');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../server/index');
 
-// describe('API Tests', () => {
-//   it('Should get a response 200 given a GET top-picks', (done) => {
-//     request(app)
-//       .get('/api/top-picks/fetch')
-//       .expect(200, done);
-//   });
+describe('API Tests', () => {
+  it('Should get a response 200 given a GET top-picks', (done) => {
+    request(app)
+      .get('/api/top-picks/fetch')
+      .expect(200, done);
+  });
 
-//   it('Should get a response 200 given a GET ultimately-bought', (done) => {
-//     request(app)
-//       .get('/api/ultimately-bought/fetch')
-//       .expect(200, done);
-//   });
+  it('Should get a response 200 given a GET ultimately-bought', (done) => {
+    request(app)
+      .get('/api/ultimately-bought/fetch')
+      .expect(200, done);
+  });
 
-//   it('Should get a response 200 given a GET ultimately-bought/fetch-all', (done) => {
-//     request(app)
-//       .get('/api/ultimately-bought/fetch-all')
-//       .expect(200, done);
-//   });
-// });
+  it('Should get a response 200 given a GET ultimately-bought/fetch-all', (done) => {
+    request(app)
+      .get('/api/ultimately-bought/fetch-all')
+      .expect(200, done);
+  });
+});
 
-// afterAll(() => {
-//   mongoose.connection.close();
-// });
+afterAll(() => {
+  mongoose.connection.close();
+});
