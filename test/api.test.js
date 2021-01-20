5f3a34d89272475abb5c9b7f7af25996645a243d
Rob <robertlopezca@gmail.com>
2021-01-20 09:37:03 -0800

    changed api tests

--- test/api.test.js
+++ test/api.test.js
@@ -5,30 +5,18 @@ describe('API Tests', () => {
   it('Should get a response 200 given a GET top-picks', (done) => {
     request(app)
       .get('/api/top-picks/fetch')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-        done();
-      });
+      .expect(200, done);
   });
 
   it('Should get a response 200 given a GET ultimately-bought', (done) => {
     request(app)
       .get('/api/ultimately-bought/fetch')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-        done();
-      });
+      .expect(200, done);
   });
 
   it('Should get a response 200 given a GET ultimately-bought/fetch-all', (done) => {
     request(app)
       .get('/api/ultimately-bought/fetch-all')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-        done();
-      });
+      .expect(200, done);
   });
 });
